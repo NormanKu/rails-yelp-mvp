12e54114d72ca69a3ef523577a06feed13a3f481
NormanKu <kunorman4@gmail.com>
2023-05-23 16:22:18 +1000

    Push

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,7 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new(review_params)
-    @review.restaurant = @restaurant
+    @review = @restaurant.reviews.build(review_params)
 
     if @review.save
       redirect_to restaurant_path(@restaurant), notice: 'Review created successfully.'
